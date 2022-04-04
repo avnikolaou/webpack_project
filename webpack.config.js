f71aa6e3fed6fa0576aaaa0296e063424aba2d59
Avraam Nikolaou <avraam.nikolaou@ordereze.com>
2022-04-04 14:51:31 +0300

    feat: add css, sass, postcss, hotreloading

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+const miniCssExtractPlugin = require('mini-css-extract-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 let mode = "development"
 
 if (process.env.NODE_ENV === 'production') {
@@ -10,6 +12,10 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.s?css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader']
+      },
+      {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
@@ -19,8 +25,11 @@ module.exports = {
     ]
   },
 
+  plugins: [new miniCssExtractPlugin()],
+
   devtool: "source-map",
   devServer: {
-    static: "./dist"
+    static: "./dist",
+    hot: true
   }
 }
